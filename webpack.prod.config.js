68c1f77953865d69e91a82fc6eaa42a3dec230f9
Felix Breuer <f.breuer94@gmail.com>
2019-11-24 23:29:52 +0100

    add service worker

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,9 +1,17 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.config');
 const { getPath } = require('./utils');
+const workboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
-	entry: [ getPath('src') ],
+	entry: [ getPath('entry-prod.js'), getPath('src') ],
 	mode: 'production',
 	devtool: 'none',
+
+	plugins: [
+		new workboxPlugin.InjectManifest({
+			swSrc: getPath('serviceworker.js'),
+			swDest: 'sw.js',
+		}),
+	],
 });
